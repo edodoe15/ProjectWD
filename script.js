93c18bdae2fe3d2b6f263f0326a11624d662b539
edodoe15 <edin.kohnic.20@size.ba>
2022-05-15 14:40:58 +0200

    kreiranje i realizacija metode PUT

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let meals = [];
+
 //GET METODA
 fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Food`)
     .then(response => {
@@ -23,6 +25,7 @@ const renderMeals = (meals) => {
                             <h3>${element.name}</h3>
                             <p>Cijena: ${element.price} KM</p>
                             <button type="button" class="btn btn-danger" onclick="deleteMeal(${element.id})">Izbriši</button>
+                            <button type="button" onclick="editMeal(${element.id})" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Uredi</button>
                         </div>
                         </div>`;
     });
@@ -38,4 +41,45 @@ const deleteMeal = (id) => {
     .then(res => {
         console.log(res);
     })
+}
+
+//PUT
+const editMeal = (elementId) => {
+    const element = meals.find(element => element.id === elementId);
+    const mealFormId = document.getElementById('jelo-id')
+    const mealFormNaziv = document.getElementById('jelo-naziv')
+    const mealFormCijena = document.getElementById('jelo-cijena')
+    const mealFormURL = document.getElementById('jelo-url')
+
+    mealFormId.value = element.id;
+    mealFormNaziv.value = element.name;
+    mealFormCijena.value = element.price;
+    mealFormURL.value = element.imageUrl;
+}
+
+const updateMeal = () => {
+    const mealFormId = document.getElementById('jelo-id').value;
+    const mealFormNaziv = document.getElementById('jelo-naziv').value;
+    const mealFormCijena = document.getElementById('jelo-cijena').value;
+    const mealFormURL = document.getElementById('jelo-url').value;
+
+    fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Food`, {
+        method: 'PUT',
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        }),
+        body: JSON.stringify ({
+            id: mealFormId,
+            name: mealFormNaziv,
+            price: mealFormCijena,
+            imageUrl: mealFormURL
+        })
+    })
+    .then(response => {
+        if(!response.ok)
+        {
+            alert('[GRESKA]');
+        }
+    })
+
 }
